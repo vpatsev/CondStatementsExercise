f8312ebcf5977e06cc82e375f8725b822a21200a
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-18 02:24:33 +0200

    Add files via upload

---
+++ 01.NumType/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine());  
+
+if (num > 0)
+{
+    Console.WriteLine("positive");
+}else if(num < 0)
+{
+    Console.WriteLine("negative");
+
+} else { Console.WriteLine("zero"); }
---
+++ 03.BiggestOfFiveNums/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+
+if (a>b && a>c && a>d && a > e)
+{
+    Console.WriteLine(a);
+} else if (a < b && b > c && b > d && b > e)
+{
+    Console.WriteLine(b);
+}  else if (c>a && c > b && c > d && c > e)
+{
+    Console.WriteLine(c);
+} else if (d > a && d > b && d > c && d > e)
+{
+    Console.WriteLine(d);
+}
+else if (e > a && e > b && e > c && e > d)
+{
+    Console.WriteLine(e);
+}
