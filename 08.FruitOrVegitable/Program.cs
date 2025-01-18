3f4fac52bcac19911969b26bbe923aad2311175b
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-18 02:26:34 +0200

    Add files via upload

---
+++ 08.FruitOrVegitable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+string product = Console.ReadLine();
+
+switch (product) {
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+        Console.WriteLine("fruit");break;
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        Console.WriteLine("vegetable"); break;
+
+    default: Console.WriteLine("unknown");break;
+}
---
+++ 09.SumSeconds/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int time = a + b + c;
+
+int minutes = time / 60;
+int seconds = time % 60;
+
+Console.WriteLine($"{minutes}:{seconds:D2}");
+
+
+
+
+
